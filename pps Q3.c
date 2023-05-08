b5fa361e941d9db2dd9db3eeaa72baaee7698f66
himaani22 <132245073+himaani22@users.noreply.github.com>
2023-05-08 10:20:11 +0530

    Add files via upload

---
+++ pps Q3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+float pi=3.14;
+int r,Perimeter,Area;
+printf("VALUE OF r = ");
+scanf("%d",&r);
+
+Perimeter = 2*pi*r;
+printf("Perimeter of circle =%d",Perimeter);
+printf("\n");
+Area = pi*r*r;
+printf("Area of circle =%d",Area);
+return 0;
+}
+    
